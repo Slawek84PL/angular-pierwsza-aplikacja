8b2568c05f60c4960f6f5e3d3331cf611a2e0a24
Slawek84PL <slapyt84@gmail.com>
2024-08-18 14:28:52 +0200

    Komunikat błedu został przeniesiony do pola errorMessage. Wartość pola jest czyszczona metodą clearErrorMessage()

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -8,11 +8,12 @@ import {Todo} from "../shared/interfaces/todo.interface";
 })
 export class TodoListComponent {
   todos: Todo[] = [];
+  errorMessage = "";
 
   addTodo(todo : string) : void {
 
     if (todo.length <= 3) {
-      alert("Zadanie powinno mieć conajmniej 4 znaki")
+      this.errorMessage = "Zadanie powinno mieć conajmniej 4 znaki";
       return;
     }
 
@@ -24,5 +25,9 @@ export class TodoListComponent {
     todo.isCompleted = !todo.isCompleted;
     // this.todos[i].isCompleted = !this.todos[i].isCompleted;
     console.log(this.todos);
+  }
+
+  clearErrorMessage() {
+    this.errorMessage = "";
   }
 }
