a4c300140f5e0eba7816519efba1036acaebd4c3
Slawek84PL <slapyt84@gmail.com>
2024-08-19 20:18:46 +0200

    Użyłem lifecycle hook DoCheck i porównanie z OnChanges

--- src/app/todo-list/todo/todo.component.ts
+++ src/app/todo-list/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {Component, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {Todo} from "../../shared/interfaces/todo.interface";
 
 @Component({
@@ -8,28 +8,34 @@ import {Todo} from "../../shared/interfaces/todo.interface";
 })
 
 // implements OnChanges
-export class TodoComponent implements OnInit {
-
+// implements OnInit
+export class TodoComponent implements OnChanges, DoCheck {
   @Input() todo!: Todo;
+
   @Input() i!: number;
   @Output() delete = new EventEmitter<void>();
   @Output() changeStatus = new EventEmitter<number>();
   openModal: boolean = false;
 
-  // ngOnChanges(changes: SimpleChanges): void {
-  //   console.log(changes);
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck został wykonany");
+  }
+
+  // constructor() {
+  //   console.log('Konstruktor ' + this.todo);
+  // }
+  //
+  // ngOnInit(): void {
+  //   console.log('OnInit: ' + this.todo);
   // }
 
-  constructor() {
-    console.log('Konstruktor ' + this.todo);
-  }
-
-  ngOnInit(): void {
-    console.log('OnInit: ' + this.todo);
-  }
-
   changeTodoStatus() {
-    this.changeStatus.emit(this.i);
+    // this.changeStatus.emit(this.i);
+    this.todo.isCompleted = !this.todo.isCompleted;
   }
 
   toggleModal(): void {
