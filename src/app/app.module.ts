5e79f57a5704a1ae1758d4fd8e2861c25073a8a7
Slawek84PL <slapyt84@gmail.com>
2024-08-22 20:51:41 +0200

    Pipe z zarejestrowanym ustawieniem local i ustawionym provider. Dzięki temu nie trzeba w pipe podawać pl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -28,7 +28,9 @@ registerLocaleData(localePl)
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pl'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
