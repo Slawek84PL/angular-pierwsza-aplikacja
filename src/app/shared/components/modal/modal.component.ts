5b4d1cb8a1844b40a1f883c17e46490801ecd117
Slawek84PL <slapyt84@gmail.com>
2024-08-25 10:34:18 +0200

    Użycie funkcji of().

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,33 +1,37 @@
-import {
-  AfterContentChecked,
-  AfterContentInit,
-  Component,
-  ContentChild,
-  ElementRef,
-  EventEmitter,
-  Input,
-  Output
-} from '@angular/core';
+import {Component, ContentChild, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {of} from "rxjs";
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements AfterContentInit, AfterContentChecked {
+export class ModalComponent implements OnInit, OnDestroy {
+
+  ngOnInit(): void {
+    of([1, 2, 3], [4, 5], [6, 7]).subscribe({
+      next: value => console.log(value),
+      error: err => console.log(err),
+      complete: () => console.log("Zakończona subskrpcja tablic")
+    });
+    of("Zasubskrybowana wiadomość").subscribe({
+      next: value => console.log(value),
+      error: err => console.log(err),
+      complete: () => console.log("Zakończona subskrpcja wartości")
+    })
+    of({obiekt: "Zasubskrybowany obiekt"}, {obiekt: "Zasubskrybowany obiekt 2"}).subscribe({
+      next: value => console.log(value),
+      error: err => console.log(err),
+      complete: () => console.log("Zakończona subskrpcja obieków")
+    })
+  }
+
+  ngOnDestroy(): void {
+  }
 
   @Input() title!: string;
   @Output() close = new EventEmitter<void>();
   @ContentChild('modalDiv') modalDiv!: ElementRef;
-  @ContentChild('check') checkbox!: ElementRef;
-
-  ngAfterContentInit(): void {
-    // console.log(this.modalDiv);
-  }
-
-  ngAfterContentChecked(): void {
-    // console.log(this.checkbox.nativeElement.checked);
-  }
 
   onClose() {
     this.close.emit();
