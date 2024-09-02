032946cda2338694af68fca1ba7cecd26f069bf9
Slawek84PL <slapyt84@gmail.com>
2024-09-02 21:23:50 +0200

    Jeżlei kontrolka ma błędy walidacyjne to pokazuje się odpowiedni komunikat

--- src/app/todo-list/add-todo-form/add-todo-form.component.ts
+++ src/app/todo-list/add-todo-form/add-todo-form.component.ts
@@ -1,16 +1,23 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Output, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-add-todo-form',
   templateUrl: './add-todo-form.component.html',
   styleUrls: ['./add-todo-form.component.css']
 })
-export class AddTodoFormComponent {
-
+export class AddTodoFormComponent implements AfterViewInit {
   @Output() addTodo = new EventEmitter<string>();
+
+  @ViewChild('form') todoForm!: NgForm;
   todoName = "";
 
-  addNewTodo() {
+  addNewTodo(form: NgForm) {
+    console.log(form);
     this.addTodo.emit(this.todoName);
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.todoForm);
   }
 }
