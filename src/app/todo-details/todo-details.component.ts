da8d59eef0243ed13a297e9431e5d0790879d4dc
Slawek84PL <slapyt84@gmail.com>
2024-09-05 08:32:41 +0200

    Przykład użycia params. Params jest observable i po zasubskrybowaniu na bieżąco możemy pobierać aktualną wartość.

--- src/app/todo-details/todo-details.component.ts
+++ src/app/todo-details/todo-details.component.ts
@@ -19,8 +19,14 @@ export class TodoDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.id = +this.route.snapshot.params['id'];
-    this.todo = this.todoService.getByIndex(this.id)
+    // this.id = +this.route.snapshot.params['id'];
+    // this.todo = this.todoService.getByIndex(this.id)
+
+    this.route.params.subscribe(params => {
+      console.log(params)
+      this.id = +params['id']
+      this.todo = this.todoService.getByIndex(this.id)
+    })
   }
 
 
